e2924d6c2b22c85f2f4ffb429ddb805fb92c13aa
jan-czerwinski <jan.ignacy.czerwinski@gmail.com>
2019-08-13 14:37:40 +0200

    Updates

---
+++ precache-manifest.4508ab02bb4ab3b1e96d86c2477330ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0713a9439b0e72735859a8d40f6c44b2",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "29de5bd1b0211d3ac396",
+    "url": "/todo-list/static/css/main.07dcf41f.chunk.css"
+  },
+  {
+    "revision": "8af4206a5c6168e122bc",
+    "url": "/todo-list/static/js/2.569bdce1.chunk.js"
+  },
+  {
+    "revision": "29de5bd1b0211d3ac396",
+    "url": "/todo-list/static/js/main.781bbfd8.chunk.js"
+  },
+  {
+    "revision": "51236a9f9133e160b8c3",
+    "url": "/todo-list/static/js/runtime~main.fb7e4658.js"
+  }
+]);
